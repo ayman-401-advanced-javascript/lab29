d2a0164168c32e68b7de0ce9737ed9eba7f12999
Ayman <aymankhawaldeh8@gmail.com>
2020-03-05 13:37:38 +0200

    done

---
+++ src/component/footer/footer.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+function Footer (){
+  return(
+    <p>
+        CopyRight@2020>>>>>> RESTy-APP
+    </p>
+  );
+}
+export default Footer;
---
+++ src/component/header/header.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {Link , NavLink} from 'react-router-dom';
+function Header (){
+  return(
+    <header>
+      <h1>
+        RESTy
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <NavLink to='/history'>Histoy</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  );
+}
+export default Header;
